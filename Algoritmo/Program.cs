00658b2bddc48558b535b6aa7d299199eb7be262
Diego Soares <diego.soares@hubconexa.com>
2022-04-29 08:42:56 -0300

    Commitando o algoritmo e o diagrama.

---
+++ Algoritmo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TesteNeon
+{
+    /*
+     * Receber um texto um texto e verificar se o mesmo é palíndromo, e retornar 1 (true) se palíndromo, e 0 (false) se não for
+     */ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("É palindromo? {0}", IsPalindromo("2002"));
+            Console.WriteLine("É palindromo? {0}", IsPalindromo("amoraroma"));
+        }
+
+        public static bool IsPalindromo(string texto)
+        {
+            //2002 - 4
+            //alala - 5 
+            //amoraroma - 9
+            //ala - 3
+
+            var esquerda = texto.Substring(0, (texto.Length / 2));
+
+            var arrayInvertido = texto.ToArray();
+
+            Array.Reverse(arrayInvertido);
+
+            var textoInvertido = new string(arrayInvertido);
+
+            var direita = textoInvertido.Substring(0, (textoInvertido.Length / 2));
+
+            return direita == esquerda;
+        }
+    }
+}
